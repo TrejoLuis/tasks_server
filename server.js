4cd94f224d4f7dddcc6a2f638eebfb9443323d71
Luis Trejo <trejorias@gmail.com>
2024-05-20 22:43:23 -0600

    feat: add MongoDB connection

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const connectDB = require('./config/dbConn.js')
+const mongoose = require('mongoose')
 const PORT = process.env.PORT ?? 3500
+
+connectDB()
 
 app.disable('x-powered-by')
 app.use(express.json())
@@ -11,6 +16,10 @@ app.use('*', (req, res) => {
   res.status(400).send('Error 404 resource not found')
 })
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+// Start express server once MongoDB is connected
+mongoose.connection.once('open', () => {
+  console.log('Connected to MongoDB')
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
 })
